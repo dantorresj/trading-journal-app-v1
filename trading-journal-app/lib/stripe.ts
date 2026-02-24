e7d6512a9268c40b8d328fc63b7976c0a121d181
Daniel <dan.torresjj@gmail.com>
2026-02-24 13:27:44 -0600

    Fix Stripe API version

--- trading-journal-app/lib/stripe.ts
+++ trading-journal-app/lib/stripe.ts
@@ -6,7 +6,7 @@ if (!process.env.STRIPE_SECRET_KEY) {
 
 // Inicializar Stripe con tu Secret Key
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2024-11-20.acacia',
+  apiVersion: '2026-01-28.clover',
   typescript: true,
 });
 
